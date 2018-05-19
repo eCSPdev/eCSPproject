18ebdd0e40b1e7258abbe2c4a9bd569591d8fe2b
LuisSantiago56 <luis.santiago56@upr.edu>
2018-05-18 20:17:07 -0400

    fixed deactivation_date on handler/Patient & Assistant

--- handler/Patient.py
+++ handler/Patient.py
@@ -4,7 +4,10 @@ from dao.Doctor import DoctorDAO
 from dao.Assistant import AssistantDAO
 from handler.RoleBase import RoleBase
 # import datetime, time
-from datetime import datetime, timezone
+# from datetime import datetime, timezone
+from datetime import datetime
+from datetime import timezone
+from datetime import timedelta
 import uuid
 
 ## Luis Santiago ##
@@ -327,7 +330,7 @@ class PatientHandler:
             else:
                 daysofgrace = form['daysofgrace']
                 print ('days fo grace : ', daysofgrace)
-                date = datetime.now(timezone.utc).astimezone().strftime('%Y-%m-%d %H:%M:%S')#datetime.datetime.fromtimestamp(changes_time)
+                date = datetime.now(timezone.utc).astimezone()#datetime.datetime.fromtimestamp(changes_time)
                 print('date', date)
                 deactivationdate = (date + datetime.timedelta(days=int(daysofgrace))).strftime('%Y-%m-%d %H:%M:%S')
                 print('deactivationdate : ', deactivationdate)

7bc09299652491665ccf8fea4e2dc13a6941815c
LuisSantiago56 <luis.santiago56@upr.edu>
2018-05-20 23:13:20 -0400

    default s3

--- config/s3config.py
+++ config/s3config.py
@@ -4,6 +4,6 @@ s3_config = {
     'aws_access_key_id' : 'AKIAIYV64ERGZOCPVXNA',
     'aws_secret_access_key' : '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',
     'region_name' : 'us-east-2',
-    'profile_name' : 'ecspdev',
+    'profile_name' : 'default',
     'bucketname' : 'ecspdoctorsegarrapatientsfiles'
 }

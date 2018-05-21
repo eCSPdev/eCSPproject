f4591e9d48f213192f6f97ff4781f84df901b67b
LuisSantiago56 <luis.santiago56@upr.edu>
2018-05-20 22:44:19 -0400

    changed profile default to ecspdev

--- config/s3config.py
+++ config/s3config.py
@@ -4,6 +4,6 @@ s3_config = {
     'aws_access_key_id' : '',
     'aws_secret_access_key' : '',
     'region_name' : 'us-east-2',
-    'profile_name' : 'default',
+    'profile_name' : 'ecspdev',
     'bucketname' : 'ecspdoctorsegarrapatientsfiles'
 }

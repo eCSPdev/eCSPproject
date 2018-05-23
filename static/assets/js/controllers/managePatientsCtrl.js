a5d5aa96645909702775b0cf168edc64a3a69e78
karla.valcarcel <karla.valcarcel@upr.edu>
2018-05-22 21:49:57 -0400

    Fixed days of grace pop-up

--- static/assets/js/controllers/managePatientsCtrl.js
+++ static/assets/js/controllers/managePatientsCtrl.js
@@ -29,8 +29,6 @@
     		$http.get('/Doctor/eCSP/PatientList?username=' + $rootScope.currentUser.username + '&token=' + $rootScope.currentUser.token) 
     		.then(function success(response) {
 
-                console.log(response);
-
         	// Search bar
         	for(var i = 0; i < response.data.Patient.length; i++) 
         	{
@@ -217,9 +215,6 @@
 
 // Popup/Modal Controller
 app.controller('ModalInstanceCtrl', ["$scope", "$rootScope", "$state", "$http", "$uibModalInstance", "chosenPatient", function ($scope, $rootScope, $state, $http, $uibModalInstance, chosenPatient) {
-
-    $scope.daysofgrace = '30';
-    console.log($scope.daysofgrace);
 
     $scope.changeStatus = function(button) {
 
